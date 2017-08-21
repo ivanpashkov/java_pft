3d5fd86719492b91ca05f3b044c13afedd399b35
ivanpashkov <30922508+ivanpashkov@users.noreply.github.com>
2017-08-21 23:48:42 +0400

    UserHelper_created

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/UserHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/UserHelper.java
@@ -4,42 +4,30 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.stqa.pft.addressbook.model.UserData;
 
-public class UserHelper {
-  private FirefoxDriver wd;
+public class UserHelper extends HelperBase {
+
 
   public UserHelper(FirefoxDriver wd) {
-    this.wd = wd;
+    super(wd);
   }
 
   public void submitUserCreation() {
-    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    click(By.xpath("//div[@id='content']/form/input[21]"));
   }
 
   public void fillUserForm(UserData userData) {
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(userData.getFirstName());
-    wd.findElement(By.name("middlename")).click();
-    wd.findElement(By.name("middlename")).clear();
-    wd.findElement(By.name("middlename")).sendKeys(userData.getMiddlename());
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(userData.getLastname());
-    wd.findElement(By.name("nickname")).click();
-    wd.findElement(By.name("nickname")).clear();
-    wd.findElement(By.name("nickname")).sendKeys(userData.getNickname());
-    wd.findElement(By.name("title")).click();
-    wd.findElement(By.name("title")).clear();
-    wd.findElement(By.name("title")).sendKeys(userData.getTitle());
-    wd.findElement(By.name("company")).click();
-    wd.findElement(By.name("company")).clear();
-    wd.findElement(By.name("company")).sendKeys(userData.getCompany());
-    wd.findElement(By.name("address")).click();
-    wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys(userData.getAddress());
+type(By.name("firstname"),userData.getFirstName());
+type(By.name("middlename"),userData.getMiddlename() );
+type(By.name("lastname"),userData.getLastname());
+type(By.name("nickname"),userData.getNickname());
+type(By.name("nickname"),userData.getNickname());
+type(By.name("title"),userData.getTitle());
+type(By.name("company"),userData.getCompany() );
+type(By.name("address"),userData.getAddress() );
+
   }
 
   public void initUserCreation() {
-    wd.findElement(By.linkText("add new")).click();
+    click(By.linkText("add new"));
   }
 }
