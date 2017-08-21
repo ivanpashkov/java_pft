6146ca156c8e20215d3aece45357bb97cc46bdf4
ivanpashkov <30922508+ivanpashkov@users.noreply.github.com>
2017-08-21 22:37:19 +0400

    beforeCreatingNavigationHelper
    finished creating GroupHelper

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -4,13 +4,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.UserData;
 
 import java.util.concurrent.TimeUnit;
 
-public class ApplicationManager extends GroupHelper{
+public class ApplicationManager {
   FirefoxDriver wd;
+  private GroupHelper groupHelper;
 
   public static boolean isAlertPresent(FirefoxDriver wd) {
     try {
@@ -25,41 +25,18 @@ public class ApplicationManager extends GroupHelper{
     wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/group.php");
+    groupHelper = new GroupHelper(wd);
     login("admin", "secret");
   }
 
   private void login(String username, String password) {
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
-  }
-
-  public void returnToGroupPage() {
-    wd.findElement(By.linkText("group page")).click();
-  }
-
-  public void submitGroupCreation() {
-    wd.findElement(By.name("submit")).click();
-  }
-
-  public void fillGroupForm(GroupData groupData) {
-    wd.findElement(By.name("group_name")).click();
-    wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
-    wd.findElement(By.name("group_header")).click();
-    wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-    wd.findElement(By.name("group_footer")).click();
-    wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-  }
-
-  public void initGroupCreation() {
-    wd.findElement(By.name("new")).click();
+     wd.findElement(By.name("user")).click();
+     wd.findElement(By.name("user")).clear();
+     wd.findElement(By.name("user")).sendKeys(username);
+     wd.findElement(By.name("pass")).click();
+     wd.findElement(By.name("pass")).clear();
+     wd.findElement(By.name("pass")).sendKeys(password);
+     wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
   }
 
   public void gotoGroupPage() {
@@ -102,11 +79,7 @@ public class ApplicationManager extends GroupHelper{
     wd.findElement(By.linkText("add new")).click();
   }
 
-  public void deleteSelectedGroups() {
-    wd.findElement(By.name("delete")).click();
-  }
-
-  public void selectGroup() {
-    wd.findElement(By.name("selected[]")).click();
+  public GroupHelper getGroupHelper() {
+    return groupHelper;
   }
 }
