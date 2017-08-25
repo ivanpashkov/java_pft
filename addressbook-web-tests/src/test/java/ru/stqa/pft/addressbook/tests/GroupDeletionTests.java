d3e0eca933eb62ba275291042d68ac104f76f53c
ivanpashkov <30922508+ivanpashkov@users.noreply.github.com>
2017-08-25 16:00:56 +0400

    реализована проверка предусловий

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -2,16 +2,20 @@ package ru.stqa.pft.addressbook.tests;
 //123
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.appmanager.TestBase;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
     
     @Test
     public void testGroupDeletion() {
-        app.getNavigationHelper().gotoGroupPage();
-      app.getGroupHelper().selectGroup();
-      app.getGroupHelper().deleteSelectedGroups();
-     app.getGroupHelper().returnToGroupPage();
+    app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+      }
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().deleteSelectedGroups();
+    app.getGroupHelper().returnToGroupPage();
     }
 
 }
