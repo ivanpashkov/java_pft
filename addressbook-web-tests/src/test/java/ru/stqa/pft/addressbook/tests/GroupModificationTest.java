c314b029a153f94b92cf078f96195ecb778966e1
ivanpashkov <30922508+ivanpashkov@users.noreply.github.com>
2017-09-03 23:39:37 +0400

    testsrunfine-BeforeUserModif
    Every test runs fine. Need to figure out how to modifyUser

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
@@ -5,23 +5,25 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.appmanager.TestBase;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupModificationTest extends TestBase {
 
   @Test
   public void testGroupModification() {
 
     app.getNavigationHelper().gotoGroupPage();
-    int before = app.getGroupHelper().GetGroupCount();
     if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
     }
-    app.getGroupHelper().selectGroup(before-1);
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    app.getGroupHelper().selectGroup(before.size()-1);
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
     app.getGroupHelper().submitModification();
     app.getGroupHelper().returnToGroupPage();
-    int after = app.getGroupHelper().GetGroupCount();
-    Assert.assertEquals(after, before);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size());
 
 
   }
