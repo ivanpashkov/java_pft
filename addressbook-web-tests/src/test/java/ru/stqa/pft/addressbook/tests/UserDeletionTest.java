0a905f24132826651767dcba68e53579385fd966
ivanpashkov <30922508+ivanpashkov@users.noreply.github.com>
2017-09-06 23:28:20 +0400

    UserModifAndUserDel

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/UserDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/UserDeletionTest.java
@@ -5,20 +5,24 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.appmanager.TestBase;
 import ru.stqa.pft.addressbook.model.UserData;
 
+import java.util.List;
+
 public class UserDeletionTest extends TestBase {
 
   @Test
   public void userDeletionTest() {
     app.getUserHelper().gotoUserPage();
-    int before = app.getUserHelper().getUserCount();
+    //int before = app.getUserHelper().getUserCount();
     if (! app.getUserHelper().isThereAUser()) {
       app.getUserHelper().createUser(new UserData("testName", "testMiddlename", "testLastname", "testNickname", "Mr", "Ololo", "testaddress", "test1"), true);
     }
-    app.getUserHelper().selectUser(before-1);
+    List<UserData> before = app.getUserHelper().getUserList();
+    app.getUserHelper().selectUser(before.size()-1);
     app.getUserHelper().deleteUser();
     app.getUserHelper().gotoUserPage();
-    int after = app.getUserHelper().getUserCount();
-    Assert.assertEquals(after, before - 1);
+    List<UserData> after = app.getUserHelper().getUserList();
+    //int after = app.getUserHelper().getUserCount();
+    Assert.assertEquals(after.size(), before.size() - 1);
 
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/UserModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/UserModificationTest.java
@@ -5,21 +5,26 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.appmanager.TestBase;
 import ru.stqa.pft.addressbook.model.UserData;
 
+import java.util.List;
+
 public class UserModificationTest extends TestBase {
 
   @Test
   public void userModificationTest() {
     app.getUserHelper().gotoUserPage();
-    int before = app.getUserHelper().getUserCount();
+    //int before = app.getUserHelper().getUserCount();
+
     if (! app.getUserHelper().isThereAUser()) {
       app.getUserHelper().createUser(new UserData("testName", "testMiddlename", "testLastname", "testNickname", "Mr", "Ololo", "testaddress", "test1"), true);
     }
+    List<UserData> before = app.getUserHelper().getUserList();
     app.getUserHelper().editUser();
     app.getUserHelper().fillUserForm(new UserData("testName3", "testMiddlename3", "testLastname3", "testNickname3", "Mr3", "Ololo3", "testaddress3", null), false);
     app.getUserHelper().confirmUserEdit();
     app.getUserHelper().gotoUserPage();
-    int after = app.getUserHelper().getUserCount();
-    Assert.assertEquals(after, before);
+    List<UserData> after = app.getUserHelper().getUserList();
+    //int after = app.getUserHelper().getUserCount();
+    Assert.assertEquals(after.size(), before.size());
   }
 
 }
