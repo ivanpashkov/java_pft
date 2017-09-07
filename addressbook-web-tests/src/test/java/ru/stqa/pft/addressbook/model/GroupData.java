cac69991ebf212001e430daca0694053681c1b08
ivanpashkov <30922508+ivanpashkov@users.noreply.github.com>
2017-09-07 13:58:00 +0400

    07group

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,12 +1,53 @@
 package ru.stqa.pft.addressbook.model;
 //123
 public class GroupData {
+  private final String id;
   private final String name;
   private final String header;
   private final String footer;
 
+
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    GroupData groupData = (GroupData) o;
+
+    if (id != null ? !id.equals(groupData.id) : groupData.id != null) return false;
+    return name != null ? name.equals(groupData.name) : groupData.name == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = id != null ? id.hashCode() : 0;
+    result = 31 * result + (name != null ? name.hashCode() : 0);
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return "GroupData{" +
+            "id='" + id + '\'' +
+            ", name='" + name + '\'' +
+            '}';
+  }
+
+  public String getId() {
+    return id;
+  }
+
   public GroupData(String name, String header, String footer) {
     this.name = name;
+    this.id = null;
+    this.header = header;
+    this.footer = footer;
+  }
+
+  public GroupData(String id, String name, String header, String footer) {
+    this.name = name;
+    this.id = id;
     this.header = header;
     this.footer = footer;
   }
@@ -23,25 +64,4 @@ public class GroupData {
     return footer;
   }
 
-  @Override
-  public String toString() {
-    return "GroupData{" +
-            "name='" + name + '\'' +
-            '}';
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-
-    GroupData groupData = (GroupData) o;
-
-    return name != null ? name.equals(groupData.name) : groupData.name == null;
-  }
-
-  @Override
-  public int hashCode() {
-    return name != null ? name.hashCode() : 0;
-  }
 }
